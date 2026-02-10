b2fd6fa1c21d1e1e652d8714af7d2128b028d3ac
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-02-10 13:48:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json, os, re, glob, time, shutil, tempfile
 from datetime import datetime, timedelta, timezone
 
 # --- 1. CONFIGURATION ---
-DOMAIN = "https://tv.cricfoot.net" 
+DOMAIN = "https://www.cricfoot.net" 
 
 # FIX: Hardcode the Timezone to Nepal (UTC+5:45). 
 LOCAL_OFFSET = timezone(timedelta(hours=5, minutes=45))
